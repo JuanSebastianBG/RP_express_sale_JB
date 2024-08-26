ed498c499d31112b423e656f0e283840c18cad9f
SrCartoons <juansebastianbernalgamboa@gmail.com>
2024-08-26 13:11:00 -0500

    Act_Conn

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
 
-echo "HGello world"
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=express-sale-bd", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
 ?>
